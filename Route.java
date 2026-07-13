4df5f5d576c7538e84d0037bf6a6eebc5791470a
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-13 10:47:41 +0800

    added attributes, constructors, and getters

--- Route.java
+++ Route.java
@@ -1,3 +1,35 @@
 public class Route {
+    private String origin;
+    private String place1;
+    private String place2;
+    private double distance;
 
+    public Route(String origin, String place1, String place2, double distance) {
+        this.origin = origin;
+        this.place1 = place1;
+        this.place2 = place2;
+        this.distance = distance;
+    }
+
+    public Route(String place1, String place2, double distance) {
+        this.place1 = place1;
+        this.place2 = place2;
+        this.distance = distance;
+    }
+
+    public String getOrigin() {
+        return this.origin;
+    }
+
+    public String getPlace1() {
+        return this.place1;
+    }
+
+    public String getPlace2() {
+        return this.place2;
+    }
+
+    public double getDistance() {
+        return this.distance;
+    }
 }
