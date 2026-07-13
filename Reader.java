36435a393256c485a1f8725793e7bbd890b9c497
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-13 12:32:48 +0800

    syntax changes

--- Reader.java
+++ Reader.java
@@ -9,11 +9,11 @@ public class Reader {
     public List<Route> readMap(String filePath) throws FileNotFoundException, IOException, NumberFormatException {
         List<Route> map = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String header = reader.readLine();
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String header = br.readLine();
             String line;
 
-            while((line = reader.readLine()) != null) {
+            while((line = br.readLine()) != null) {
                 String[] data = line.split(",");
 
                 if (data.length >= 4) {
@@ -21,7 +21,6 @@ public class Reader {
                     String place1 = data[1].trim();
                     String place2 = data[2].trim();
                     double distance = Double.parseDouble(data[3].trim());
-
                     Route r = new Route(origin, place1, place2, distance);
                     map.add(r);
                 }
@@ -34,18 +33,17 @@ public class Reader {
     public List<Route> readOffices(String filePath) throws FileNotFoundException, IOException, NumberFormatException {
         List<Route> offices = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String header = reader.readLine();
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String header = br.readLine();
             String line;
 
-            while((line = reader.readLine()) != null) {
+            while((line = br.readLine()) != null) {
                 String[] data = line.split(",");
 
                 if (data.length >= 3) {
                     String place1 = data[0].trim();
                     String place2 = data[1].trim();
                     double distance = Double.parseDouble(data[2].trim());
-
                     Route r = new Route(place1, place2, distance);
                     offices.add(r);
                 }
