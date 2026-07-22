34379807feb4963fa8e08f66f43936cb14bd4398
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-22 12:02:05 +0800

    added constructor and methods

--- Graph.java
+++ Graph.java
@@ -1,3 +1,38 @@
-public class Graph {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Graph {
+    private Map<Vertex, List<Edge>> adjacencyList;
+
+    public Graph() {
+        this.adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(Vertex v) {
+        adjacencyList.putIfAbsent(v, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex source, Vertex destination, double weight) {
+        addVertex(source);
+        addVertex(destination);
+        adjacencyList.get(source).add(new Edge(source, destination, weight));
+        adjacencyList.get(destination).add(new Edge(destination, source, weight));
+    }
+
+    public List<Edge> getNeighbors(Vertex v) {
+        return adjacencyList.getOrDefault(v, Collections.emptyList());
+    }
+
+    public int getVertexCount() {
+        return adjacencyList.size();
+    }
+
+    /*
+    FOR DEBUG PURPOSES ONLY
+    public Set<Vertex> getVertices() {
+        return adjacencyList.keySet();
+    }*/
 }
