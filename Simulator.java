17765848e668d25c91d4e4f643759b37dfb30930
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-22 12:40:20 +0800

    added some attributes and solver methods

--- Simulator.java
+++ Simulator.java
@@ -1,3 +1,66 @@
-public class Simulator {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Simulator {
+    private List<Edge> bestRoute;
+    private double shortestDistance;
+    //other attributes
+
+    public Simulator() {
+        //attribute instantiation (not including bestRoute and shortestDistance)
+    }
+
+    //other getters and setters
+
+    public List<Edge> getBestRoute() {
+        return this.bestRoute;
+    }
+
+    public double getShortestDistance() {
+        return this.shortestDistance;
+    }
+
+    //other methods
+
+    public void findShortestPath(Graph g, Vertex start) {
+        bestRoute = new ArrayList<>();
+        shortestDistance = Double.POSITIVE_INFINITY;
+
+        Set<Vertex> visited = new HashSet<>();
+        visited.add(start);
+
+        List<Edge> currentRoute = new ArrayList<>();
+        findPath(g, start, visited, currentRoute);
+    }
+
+    public void findPath(Graph g, Vertex current, Set<Vertex> visited, List<Edge> currentRoute) {
+        if(visited.size() == g.getVertexCount()) {
+            double totalDistance = 0.0;
+
+            for(Edge e : currentRoute)
+                totalDistance += e.getWeight();
+
+            if(totalDistance < shortestDistance) {
+                shortestDistance = totalDistance;
+                bestRoute = new ArrayList<>(currentRoute);//bestRoute = (List<Edge>) currentRoute.clone();
+            }
+        }
+        else {
+            for(Edge e : g.getNeighbors(current)) {
+                Vertex next = e.getDestination();
+
+                if(!visited.contains(next)) {
+                    visited.add(next);
+                    currentRoute.add(e);
+
+                    findPath(g, next, visited, currentRoute);
+
+                    currentRoute.remove(currentRoute.size() - 1);
+                    visited.remove(next);
+                }
+            }
+        }
+    }
 }
