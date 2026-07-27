5dcc64ac3a634aeec8a70a1c0aa29989e9e1b61c
dihhcoder <kendrickconstantino47@gmail.com>
2026-07-27 14:34:01 +0800

    comments

--- Simulator.java
+++ Simulator.java
@@ -20,12 +20,16 @@ public class Simulator {
         List<Edge> toMail;
         double maildist;
         
-        
+        //gets csv input
         office = Input.readOfficeFileLocation(keyboard);
         map = Input.readMapFileLocation(keyboard);
+        //starts the loop
         do{
+            //gets the starting post office
             start = new Vertex(Input.readStartingOffice(keyboard, office));
             first = true;
+
+            //gets destinations then puts them in an unfiltered deque
             while(first || !unfiltered.isEmpty()){
                 mailInput = getMailCount(keyboard);
                 dest = Input.readDestinations(keyboard, map, mailInput);
@@ -34,17 +38,21 @@ public class Simulator {
                 }
                 dest.clear();
 
+                
                 first = false;
                 origin = null;
 
+                //gets origin for filtering
                 for(i = 0; i < map.size() && origin == null; i++){
                     if(map.get(i).getPlace1().equalsIgnoreCase(start.name())){
                         origin = map.get(i).getOrigin();
                     }
                 }
 
+                //makes a list for rejected destinations for current origin
                 rejected = new ArrayList<>();
 
+                //filtering process
                 while(!unfiltered.isEmpty()){
                     check = false;
                     String s = unfiltered.poll();
@@ -63,34 +71,35 @@ public class Simulator {
                     }
                 }
                 
+                //adds rejected back to unfiltered
                 for(String s: rejected){
                     unfiltered.add(s);
                 }
+                //clears rejected list
                 rejected.clear();
-                //just to see what is filtered
+                //just to see what is filtered and unfiltered
                 System.out.println(filtered);
                 System.out.println(unfiltered);
 
+                
                 g = new Graph();
+                
+                //adds route to graph if route has filtered dest in either the place1 or place2 on nonlinear map
                 for (i = 0; i < map.size(); i++) {
                     Route r = map.get(i);
-
                     if (r.getOrigin().equalsIgnoreCase(origin)) {
-
                         isPlace1Valid = r.getPlace1().equalsIgnoreCase(start.name());
                         for (String f : filtered) {
                             if (f.equalsIgnoreCase(r.getPlace1())) {
                                 isPlace1Valid = true;
                             }
                         }
-
                         isPlace2Valid = r.getPlace2().equalsIgnoreCase(start.name());
                         for (String f : filtered) {
                             if (f.equalsIgnoreCase(r.getPlace2())) {
                                 isPlace2Valid = true;
                             }
                         }
-
                         if (isPlace1Valid && isPlace2Valid) {
                             g.addEdge(new Vertex(r.getPlace1()), new Vertex(r.getPlace2()), r.getDistance());
                             g.addEdge(new Vertex(r.getPlace2()), new Vertex(r.getPlace1()), r.getDistance());
@@ -98,7 +107,7 @@ public class Simulator {
                     }
                 }
 
-
+                //runs the solver
                 solver.findShortestCycle(g, start);
                 toMail = solver.getBestCycle();
                 maildist = solver.getBestCycleDistance();
@@ -110,8 +119,9 @@ public class Simulator {
                 }
                 System.out.println(maildist);
 
-
+                //to get the next city
                 post = new ArrayList<>();
+                //gets next possible post office by origin
                 for(String s: unfiltered){
                     for(i = 0; i < map.size(); i++){
                         Route r = map.get(i);
@@ -127,6 +137,7 @@ public class Simulator {
                 }
                 g = new Graph();
 
+                //adds route to graph if post office is located in either place1 or place 2 on the postoffice.csv
                 for(i = 0; i < office.size(); i++){
                     Route r = office.get(i);
                     boolean is1Valid = r.getPlace1().equalsIgnoreCase(origin + " Post Office") || post.contains(r.getPlace1());
@@ -138,19 +149,22 @@ public class Simulator {
                         g.addEdge(place2, place1, r.getDistance());
                     }
                 }
+
+                //runs solver
                 Vertex currentOffice = new Vertex(origin + " Post Office"); 
                 solver.findShortestPath(g, currentOffice);
+
+                //swaps cities if next city is available else no more
                 if(!solver.getBestPath().isEmpty()){
                     String nextCity = solver.getBestPath().get(0).getDestination().name();
                     String justCity = nextCity.replaceAll("(?i)\\sPost\\sOffice", "").trim();
                     System.out.println("Next City: " + justCity);
                     start = new Vertex(nextCity);
-
                 } else {
                     System.out.println("No city");
                 }
+                //clears filter for next use
                 filtered.clear();
-                
             }
             over = runAgain(keyboard);      
         } while(!over);
