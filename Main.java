e80ce9254a4d544bdbecb4c75ed2ab7e3f04ab96
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-27 20:31:51 +0800

    syntax changes

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
         boolean isRunning = true;
 
         while(isRunning) {
-            System.out.println("+-------------------------------------+"); // temporary intro display
-            System.out.println("|   Welcome to Arrow Mail Simulator   |");
-            System.out.println("+-------------------------------------+");
+            System.out.println("+---------------------------------------+"); // temporary intro display
+            System.out.println("|   Welcome to Arrow Mail Simulator 2   |");
+            System.out.println("+---------------------------------------+");
             System.out.println("Please select an option:");
             System.out.println("1. Start Simulation");
             System.out.println("2. Exit");
@@ -22,7 +22,10 @@ public class Main {
                     break;
                 case 2:
                     isRunning = false;
-                    System.out.println("Exiting the program. Goodbye!"); // temporary exit message
+                    System.out.println();
+                    System.out.println("+----------------------------------------+");
+                    System.out.println("| Work hard, Mailman! See you next time! |");
+                    System.out.println("+----------------------------------------+"); // temporary exit message
                     break;
             }
         }
